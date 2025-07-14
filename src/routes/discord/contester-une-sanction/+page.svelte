797ca8ab9bed59ab2ec39e0ec0b3f9929d5b69a6
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-07-14 15:21:57 +0200

    feat: integrate Lottie animation for failure indication and use div gap instead of margin bottom

--- src/routes/discord/contester-une-sanction/+page.svelte
+++ src/routes/discord/contester-une-sanction/+page.svelte
@@ -1,15 +1,28 @@
 <script>
-	import { GitFork, ShieldX } from 'lucide-svelte';
+	import { ShieldX } from 'lucide-svelte';
 	import Button from '../../../components/Button.svelte';
+	import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
 </script>
 
 <main>
-    <img src="/assets/checkmark_fail.png" alt="Checkmark with an x" />
-	<h1>Contester une sanction</h1>
-    <br><br>
-	<p>Tu as été banni du Discord de Papillon et tu penses que ta sanction est injustifiée ?</p>
-    <p>Envoie nous un e-mail avec ton nom d'utilisateur Discord ainsi que ta version des faits.<br>Nous examinerons ta demande dans les plus brefs délais.</p>
-	<Button value="Envoyer une contestation" icon={ShieldX} variant="light" href="mailto:support@papillon.bzh" />
+	<div class="container">
+		<LottiePlayer
+			src="/lottie/fail.json"
+			autoplay="{true}"
+			loop="{false}"
+			controls={false}
+			renderer="svg"
+			background="transparent"
+			height={200}
+			width={200}
+		/>
+		<h1>Contester une sanction</h1>
+		<div class="longText">
+			<p>Tu as été banni du Discord de Papillon et tu penses que ta sanction est injustifiée ?</p>
+			<p>Envoie nous un e-mail avec ton nom d'utilisateur Discord ainsi que ta version des faits.<br>Nous examinerons ta demande dans les plus brefs délais.</p>
+		</div>
+		<Button value="Envoyer une contestation" icon={ShieldX} variant="light" href="mailto:support@papillon.bzh" />
+	</div>
 </main>
 
 <style>
@@ -26,13 +39,11 @@
 	}
 
 	img {
-		width: 200px;
-		height: 200px;
-		padding: 28px;
+		width: 144px;
+		height: 144px;
 	}
 
 	h1 {
-		margin-top: 1rem;
 		font-size: 1.8rem;
 		color: var(--papillon-primary);
 	}
@@ -40,7 +51,19 @@
 	p {
 		color: rgba(var(--color-text-rgb), 0.4);
 		font-size: 1rem;
-		margin-bottom: 30px;
-        text-align: center;
+			text-align: center;
+	}
+
+	.longText {
+			display: flex;
+			flex-direction: column;
+			gap: 5px;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 20px;
 	}
 </style>
